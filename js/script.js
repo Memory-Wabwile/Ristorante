783ba74681c00210c1503cfd6f815f87a7a31869
Memory-Wabwile <memorywabwile@gmail.com>
2023-03-24 01:20:25 +0300

    add functionality to the search and the remove icons

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+// simply means on click of the search icon toggle/show the search form
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+// simply means on click of the close icon remove the search form
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
